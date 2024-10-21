48976c82ab9e62b72a17b317137418f6d1961f8d
Scprondation <scp80864@gmail.com>
2024-10-21 12:55:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+const clickCounter = document.getElementById('click-counter');
+const progressBar = document.getElementById('progress');
+const clickCircle = document.getElementById('click-circle');
+const shop = document.getElementById('shop');
+let clicks = 0;
+let clicksNeeded = 100;
+let clickBonus = 1;
+
+// Обновление счетчика кликов
+function updateCounter() {
+  clickCounter.textContent = clicks;
+}
+
+// Обновление прогресс-бара
+function updateProgressBar() {
+  const progressWidth = (clicks % clicksNeeded) / clicksNeeded * 100;
+  progressBar.style.width = progressWidth + '%';
+}
+
+// Функция обработки клика по кругу
+clickCircle.addEventListener('click', () => {
+  clicks += clickBonus; // Добавляем бонусные клики
+  updateCounter();
+  updateProgressBar();
+
+  // Проверка на достижение цели
+  if (clicks >= clicksNeeded) {
+    clicksNeeded *= 5;
+    
+    updateProgressBar();
+  }
+});
+
+// Функция обработки покупки
+function buyItem(item) {
+  const price = parseInt(item.dataset.price);
+
+  if (clicks >= price) {
+    clicks -= price;
+    updateCounter();
+
+    clickBonus++;
+    item.querySelector('button').textContent = `${clickBonus} клик +1 за ${price * 2}`;
+    item.dataset.price = price * 2; // Увеличиваем цену для следующей покупки
+    updateProgressBar();
+  } else {
+    alert('Недостаточно кликов!');
+  }
+}
+
+// Добавление обработчиков кликов на кнопки в магазине
+shop.addEventListener('click', (event) => {
+  if (event.target.tagName === 'BUTTON') {
+    const item = event.target.closest('.item');
+    buyItem(item);
+  }
+});
+
+// Инициализация счетчика и прогресс-бара
+updateCounter();
+updateProgressBar();
